41451c5e616f5032c1c33b1b979beb5f2df1e3da
Jungwoo Yang <jungwoo.yang@qraftec.com>
2022-03-16 14:45:39 +0900

    add install file

--- install.sh
+++ install.sh
@@ -3,6 +3,44 @@ set -e
 #set -x
 
 PREFIX=$HOME
+TARGET=$HOME
 
-FileArray=(".vimrc", ) 
+FileArray=( 
+    ".vimrc"
+    ".vimrc.coc"
+    ".zshrc"
+    ".tmux.conf"
+    "jwcolors.vim"
+    ".pdbrc"
+    ".ipdb"
+) 
 
+for f in "${FileArray[@]}"
+do
+    echo "copying $f to $TARGET"
+    cp $f $TARGET
+done
+
+echo "installing ohmyzsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "copying jungwoo zsh theme"
+cp jungwoo.zsh-theme ~/.oh-my-zsh/themes/
+
+echo "installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "installing zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "installing vim plugins..."
+vim +'PlugInstall --sync' +qa
+
+echo "installing coc-vim plugins..."
+vim +'CocUpdateSync' +qa
+
+echo "copying coc-settings..."
+cp ./coc-settings.json ~/.vim/
+
+echo "installing tmux plugins..."
+~/.tmux/plugins/tpm/bin/install_plugins
