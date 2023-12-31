12ce66b359289e134076bb9e740539eb698cfee4
Jungwoo Yang <jwyang0213@gmail.com>
2023-12-31 13:42:22 +0900

    feat: add tabstop

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,14 @@
 -- numbers for left columns
-vim.o.number = true
-vim.o.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.termguicolors = true
 
 vim.api.nvim_create_autocmd("FocusGained", {
     pattern = "*",
