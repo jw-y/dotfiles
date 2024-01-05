85b37457fb9a7b32425d658cbbfc2e05b94b41a0
Jungwoo <jungwoo.yang@qraftec.com>
2024-01-05 11:06:16 +0900

    feat: add NvimTree

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,18 @@
 return {
     {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup{
+                disable_netrw = true,
+            }
+        end,
+    },
+    {
         "nvim-lualine/lualine.nvim",
         opts = {
             theme = "catppuccin",
