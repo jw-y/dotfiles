20e31e7f67179fad68a4bb3ec302a93d255d6502
Jungwoo <jungwoo.yang@qraftec.com>
2024-01-08 11:04:36 +0900

    feat: add nvim-treesitter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -29,5 +29,23 @@ return {
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
         }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = {
+                    "c", "lua", "vim", "vimdoc", "query",
+                    "python", "javascript",
+                    "html", "markdown", "toml", "yaml",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
     }
 }
