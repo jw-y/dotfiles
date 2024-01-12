d6b1b510602eb537612cf8c2cd73730e135b06ed
Jungwoo <jungwoo.yang@qraftec.com>
2024-01-12 12:39:53 +0900

    ch: override colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,13 @@ return {
         config = {
             flavour = "mocha",
             transparent_background = false,
+            color_overrides = {
+				mocha = {
+					base = "#101016", -- "#000000",
+					mantle = "#000000",
+					--crust = "#000000",
+				},
+			},
         }
     },
 }
